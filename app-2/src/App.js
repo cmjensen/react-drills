736fda8a369480cb916fd2cbc834c4e6a928bfc3
Christie Jensen <christiejensen@Christies-MacBook-Air.local>
2021-01-21 14:12:17 -0700

    app2 built

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { Component }from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      famBam: ['kason', 'nathan', 'christie', 'lizzie', 'katie', 'junie', 'adam']
+    }
+  }
+
+  render() {
+    let famBam = this.state.famBam.map((e, i) => {
+      return <h2 key={i}>{e}</h2>;
+    });
+
+    return (
+      <div className='App'>{famBam}</div>
+    )
+  }
 }
 
 export default App;
